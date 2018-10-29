c770b2a71626c1396b70e36fa46695c27d877c3e
Daniel Quintanilla <daniel.quintanilla@shopgate.com>
2018-10-29 09:50:37 -0500

    updated peer dependencies

--- frontend/portals/ShareButton/spec.jsx
+++ frontend/portals/ShareButton/spec.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import ShareButton from './index';
 
-jest.mock('../../components/ShareButton', () => () => <div>ShareButton</div>);
+// eslint-disable-next-line require-jsdoc
+const MockedShareButton = () => (<div>ShareButton</div>);
+jest.mock('../../components/ShareButton', () => MockedShareButton);
 
 describe('ShareButton', () => {
+  // eslint-disable-next-line global-require
+  const ShareButton = require('./index').default;
   it('should render ShareButton', () => {
     const component = mount(<ShareButton />);
+    expect(component.find(MockedShareButton).exists()).toBe(true);
     expect(component).toMatchSnapshot();
   });
 });
