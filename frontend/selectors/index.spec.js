db6ab5e27495d8908cb82878f8fc9aecacc4a900
Daniel Quintanilla <daniel.quintanilla@shopgate.com>
2018-10-30 08:18:52 -0500

    Corrected Selector test

--- frontend/selectors/index.spec.js
+++ frontend/selectors/index.spec.js
@@ -2,18 +2,45 @@ import { getShareParams } from './index';
 
 describe('Selectors', () => {
   describe('getShareParams', () => {
-    it('should return null if no product is available', () => {
-      const result = getShareParams({ currentProduct: {} });
+    it('should return null when product Id not found', () => {
+      const state = {
+        product: {
+          currentProduct: {
+            productId: 'bar',
+          },
+          productsById: {
+            foo: {
+              productData: {
+                name: 'title',
+                featuredImageUrl: 'imageURL',
+                productUrl: 'deeplink',
+              },
+            },
+          },
+        },
+      };
+      const result = getShareParams(state);
       expect(result).toEqual(null);
     });
     it('should return parameters for share command', () => {
-      const product = {
-        name: 'title',
-        featuredImageUrl: 'imageURL',
-        productUrl: 'deepLink',
+      const state = {
+        product: {
+          currentProduct: {
+            productId: 'foo',
+          },
+          productsById: {
+            foo: {
+              productData: {
+                name: undefined,
+                featuredImageUrl: undefined,
+                productUrl: undefined,
+              },
+            },
+          },
+        },
       };
-      const result = getShareParams({ currentProduct: product });
-      expect(result).toEqual(product);
+      const result = getShareParams(state);
+      expect(result).toEqual(state.product.productsById.foo.productData);
     });
   });
 });
