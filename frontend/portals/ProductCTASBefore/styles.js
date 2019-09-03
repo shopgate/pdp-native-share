e6be98de642b30d65e5b2ed76b8f68ee5355637a
Patrick Scherer <patrick.scherer@shopgate.com>
2019-09-03 14:10:35 +0200

    Fix position for ios on PWA 6.7.x

--- frontend/portals/ProductCTASBefore/styles.js
+++ frontend/portals/ProductCTASBefore/styles.js
@@ -2,7 +2,7 @@ import { css } from 'glamor';
 const iOSButtons = css({
   position: 'absolute',
   right: 62,
-  top: -30,
+  top: -20,
   display: 'flex',
   justifyContent: 'flex-end',
   alignItems: 'center',
